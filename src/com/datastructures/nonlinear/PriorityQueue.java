b0cb3e6f9f0ad70b3c36fbb2e9f998041392f202
Sasi Krishna <sk@recruitfox.com>
2018-05-08 00:14:21 +0530

    added comments for PriorityQueue

--- src/com/datastructures/nonlinear/PriorityQueue.java
+++ src/com/datastructures/nonlinear/PriorityQueue.java
@@ -3,6 +3,12 @@ package com.datastructures.nonlinear;
 import java.util.Arrays;
 
 /**
+ * <code>PriorityQueue</code> is an implementation Priority Queue data structure using Heap data structure. 
+ * This is max heap implementation so root node is greater than its child node.
+ * 
+ * <p>
+ * 	 Time complexities for insertion, deleteMax, getMax are O(logn), O(logn) and O(1) respectively.	
+ * </p>
  * 
  * @author Sasi on 06-May-2018, 11:36:04 pm
  */
@@ -37,13 +43,21 @@ public class PriorityQueue {
 			throw new Exception("Queue is empty");
 		
 		int max = getMax();
-		
-		this.array[0] = this.array[this.count--];
+		this.array[0] = this.array[(this.count-1)];
+		this.array[(this.count-1)] = 0;
+		this.count--;
 		percolateDown(0);
 		
 		return max;
 	}
 
+	/**
+	 * <code>percolateUp</code> method starts at the bottom of heap, crawls upwards and swaps 
+	 * 	child, parent elements if max heap condition is not satisfied.
+	 * 
+	 * <p>Insertion of new element happens at after last leaf node. We'll call <code>percolateUp</code> to heapify tree.</p> 
+	 * @param index
+	 */
 	private void percolateUp(int index){
 		int parentIndex = getParentIndex(index);
 		
@@ -55,7 +69,14 @@ public class PriorityQueue {
 			percolateUp(parentIndex);
 		}
 	}
-
+	
+	/**
+	 * <code>percolateUp</code> method starts at the top of heap, crawls downwards and swaps 
+	 * 	parent, child elements if max heap condition is not satisfied.
+	 * 
+	 * <p>Since this is a max heap, deleteMax should remove root node. So we'll copy last leaf node value to root node call <code>percolateDown</code> method to heapify tree.</p> 
+	 * @param index
+	 */
 	private void percolateDown(int index) {
 		
 		if(index == this.count)
@@ -64,18 +85,20 @@ public class PriorityQueue {
 		int leftIndex = getLeftChildIndex(index), 
 				rightIndex = getRightChildIndex(index), maxIndex;
 		
-		if(leftIndex != -1 && this.array[index] < this.array[leftIndex]){
+		if(leftIndex != -1 && leftIndex <= this.count 
+				&& this.array[index] < this.array[leftIndex]){
 			maxIndex = leftIndex;
 		}else{
 			maxIndex = index;
 		}
 		
-		if(rightIndex != -1 && this.array[maxIndex] < this.array[rightIndex]){
+		if(rightIndex != -1 && rightIndex <= this.count && 
+				this.array[maxIndex] < this.array[rightIndex]){
 			maxIndex = rightIndex;
 		}
 		
 		if(maxIndex != index){
-			//Swap elements at index and maxIndex
+			//Swaping elements at index and maxIndex
 			int temp = this.array[index];
 			this.array[index] = this.array[maxIndex];
 			this.array[maxIndex] = temp;
@@ -93,7 +116,7 @@ public class PriorityQueue {
 	
 	private int getLeftChildIndex(int parentIndex){
 		
-		if(parentIndex <= 0){
+		if(parentIndex < 0){
 			return -1;
 		}
 		
@@ -102,7 +125,7 @@ public class PriorityQueue {
 	
 	private int getRightChildIndex(int parentIndex){
 		
-		if(parentIndex <= 0){
+		if(parentIndex < 0){
 			return -1;
 		}
 		
@@ -138,6 +161,9 @@ public class PriorityQueue {
 		System.out.println("Max element: " + queue.getMax());
 		
 		System.out.println("Deleting max element: " + queue.deleteMax());
+		System.out.println(queue);
+		
+		queue.insert(100);
 		System.out.println(queue);
 	}
 }
