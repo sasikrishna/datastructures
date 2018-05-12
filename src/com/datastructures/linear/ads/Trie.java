bfe141809d588ebf6522e121b158c98108f9553e
Sasi Krishna <sk@recruitfox.com>
2018-05-12 16:57:24 +0530

    added implementation for Trie data structure

---
+++ src/com/datastructures/linear/ads/Trie.java
@@ -0,0 +1,83 @@
+package com.datastructures.linear.ads;
+
+/**
+ *  Trie class represents implementation of Trie data structure.
+ *  <br>Trie data structure forms structure hierarchy by having a child array (which contains next letters) for each letter in the given key which makes it easy for searching operation.
+ *  <br><br>
+ *  Time complexity for adding key to Trie - O(n)<br>
+ *  Time complexity for searching key - O(n) where n is length of key
+ *  
+ * @author Sasi on 12-May-2018, 3:58:55 pm
+ */
+public class Trie {
+	
+	static TrieNode root = new TrieNode();
+	private static final int ALPHABETS_SIZE = 26;
+	
+	static class TrieNode{
+		TrieNode[] trie = new TrieNode[ALPHABETS_SIZE];    //Initializing trieNode with total alphabets size
+		boolean isEndOfWord = false;                      // Specifies this is last letter in that word
+		
+		public TrieNode(){
+			for(int i=0;i<ALPHABETS_SIZE;i++){
+				trie[i] = null;
+			}
+		}
+	}
+	
+	/**
+	 * 
+	 * @param key
+	 */
+	public static void insert(String key){
+		
+		TrieNode tempRoot = root;
+		for(int i=0;i<key.length();i++){                  // Iterating through all characters in each key and create a tries nodes if not present
+
+			int index = key.charAt(i) - 'a';              // Getting relative index from small 'a'
+			if(null ==  tempRoot.trie[index]){
+				tempRoot.trie[index] = new TrieNode();    // Creating new trie node if given letter is not present in exisitng trie 
+			}
+			tempRoot =  tempRoot.trie[index];
+		}
+		
+		tempRoot.isEndOfWord = true;                      // Marking last trie node as isEndOfWord true which is helpful to determine if word exsits or not 
+	}
+
+	/**
+	 * 
+	 * @param key
+	 * @return
+	 */
+	public static boolean search(String key){
+		
+		TrieNode tempRoot = root;
+		for(int i=0;i<key.length();i++){
+			int index = key.charAt(i) - 'a';              
+			if(null == tempRoot.trie[index]){
+				return false;
+			}
+			tempRoot =  tempRoot.trie[index];
+		}
+		
+		if(tempRoot.isEndOfWord){
+			return true;
+		}
+		
+		return false;
+	}
+	
+	public static void main(String[] args) {
+		String[] keys = new String[]{"c", "cpp", "java", "python", "perl", "sql", "golang", "erlang"};
+		
+		for(String key:keys){
+			Trie.insert(key);
+		}
+		
+		System.out.println("Java key exists: " + Trie.search("java"));
+		System.out.println("Hadoop key exists: " + Trie.search("hadoop"));
+		System.out.println("Erlang key exists: " + Trie.search("erlang"));
+		System.out.println("CPP key exists: " + Trie.search("cpp"));
+		System.out.println("Scala key exists: " + Trie.search("scala"));
+	}
+}
